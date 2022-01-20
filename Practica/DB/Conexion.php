4ef7ede5000f356f80ec238243d2f8bef241a0c4
FavioL2 <53454046+FavioL2@users.noreply.github.com>
2022-01-20 10:24:42 -0700

    Update Conexion.php

--- Practica/DB/Conexion.php
+++ Practica/DB/Conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$conexion=mysqli_connect("localhost", "root","asdasdF17","pibd")or die("Problemas con la conexión");
+	$conexion=mysqli_connect("localhost", "root","****","pibd")or die("Problemas con la conexión");
 	$Paises= mysqli_query($conexion,"SELECT * FROM Paises;");
 	$numPaises =mysqli_num_rows($Paises);
 ?>
