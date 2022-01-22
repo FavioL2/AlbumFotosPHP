cf0d40098e233e7ac70cfb517834691cdedbf2bd
FavioL2 <53454046+FavioL2@users.noreply.github.com>
2022-01-22 11:31:56 -0700

    Delete AgregarFotosBD.php

--- Practica/DB/AgregarFotosBD.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	include('Conexion.php');
-	session_start();	
-	$insertar=mysqli_query($conexion,"Insert into Fotos(IdFoto,Titulo, Descripcion, Fecha, Pais, Album, Alternativo, FRegistro,Foto,IdUsuario,Estilo) VALUES ('Null','".$_POST['Titulo']."','".$_POST['Descripcion']."','".$_POST['Fecha']."','".$_POST['Pais']."','".$_POST['Album']."','".$_POST['Alternativo']."','".$_POST['FRegistro']."','".$_POST['Foto']."','".$_SESSION['Id']."','".$_POST['Estilo']."');");
-header("Location: ../HTML/Albumes.php");
-?>
